6c1cca57340d76321c64ff1416c31b0242196d7f
psh031406 <229868975+psh031406@users.noreply.github.com>
2025-09-11 19:17:56 +0900

    hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("kumoh national institute of technology.")
 
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
